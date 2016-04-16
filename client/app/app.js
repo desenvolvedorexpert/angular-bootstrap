c669ad04899edc73693de2063699105f4aad61ea
Antônio Milesi Bastos <antonio@milesibastos.com>
2016-04-16 14:56:01 -0300

    npm install angular-scroll

--- client/app/app.js
+++ client/app/app.js
@@ -8,14 +8,19 @@ import AppComponent from './app.component';
 import angulartics from 'angulartics';
 import angularticsGoogle from 'angulartics-google-analytics';
 import 'font-awesome-loader';
+import 'angular-scroll';
 
 angular.module('app', [
     uiRouter,
     angulartics,
     angularticsGoogle,
+    'duScroll',
     Common.name,
     Components.name
   ])
+  .value('duScrollDuration', 1250)
+  .value('duScrollOffset', 50)
+  .value('duScrollEasing', function (t) { return t<0.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t; }) // https://gist.github.com/gre/1650294
   .config(($locationProvider) => {
     'ngInject';
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
